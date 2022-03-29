d9bb746d6fa1a310534b19be14cc681c03bd1d7b
vanillovin <vanillovin@gmail.com>
2022-03-29 23:40:53 +0900

    Add Note, Todo, Video Components

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+import { VideoComponent } from './components/page/item/video.js';
+import { TodoComponent } from './components/page/item/todo.js';
+import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { PageComponent } from './components/page/page.js';
 
@@ -11,6 +14,15 @@ class App {
     // 생성자 안에는 아직 사용자에게 데이터를 받는 다이얼로그가 없으므로 임의
     const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
     image.attachTo(appRoot, 'beforeend');
+
+    const video = new VideoComponent('Video Title', 'https://youtu.be/SNBaIAvKxU4');
+    video.attachTo(appRoot, 'beforeend');
+
+    const note = new NoteComponent('Note Title', 'Note Body');
+    note.attachTo(appRoot, 'beforeend');
+
+    const todo = new TodoComponent('Todo Title', 'Todo Item');
+    todo.attachTo(appRoot, 'beforeend');
   }
 }
 
---
+++ src/components/page/item/video.ts
@@ -0,0 +1,54 @@
+import { BaseComponent } from '../../component.js';
+
+export class VideoComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section class="video">
+            <div class="video__player">
+              <iframe class="video__iframe"></iframe>
+            </div>
+            <h3 class="video__title"></h3>
+          </section>`);
+
+    const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
+    iframe.src = this.convertToEmbeddedURL(url); // url -> videoId -> embed
+
+    // URL을 비디오 Id로 변환해서 우리가 이런 형태의 임베디드 코드로 변환해줘야 함
+    const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
+    titleElement.textContent = title;
+  }
+
+  // input
+  // https://www.youtube.com/watch?v=K3-jG52XwuQ
+  // https://youtu.be/K3-jG52XwuQ
+  // output
+  // https://www.youtube.com/embed/K3-jG52XwuQ
+  // 정규표현식 Regex
+  // https://regexr.com/5l6nr
+  
+  // url을 받아 임베디드용으로 변환된 string을 리턴하는 내부적으로 쓰이는 함수 
+  // [a-zA-Z0-9-] => [a-zA-Z0-9(-|_)] 이렇게 하면 언더바인것도 가져올수 있어요!
+  private convertToEmbeddedURL(url: string): string {
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+
+    console.log(match);
+
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      // 임베디드용 주소 리턴
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    // placeholder나 지원하지 않는 URL이라고 사용자에게 impormative한 좋은 정보를 줄 수 있음
+    return url;
+  }
+}
+
+  // <iframe 
+  //   width="1077" 
+  //   height="615" 
+  //   src = "https://www.youtube.com/embed/SNBaIAvKxU4"
+  //   title = "YouTube video player"
+  //   frameborder = "0"
+  //   allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+  //   allowfullscreen
+  // ></iframe>
