3b5c74107d29ec4ec401464c385d171e6e807c90
vanillovin <vanillovin@gmail.com>
2022-03-30 21:59:56 +0900

    Inject PageItemConstructor to the PageComponent for testability

--- src/app.ts
+++ src/app.ts
@@ -2,36 +2,30 @@ import { VideoComponent } from './components/page/item/video.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { Composable, PageComponent } from './components/page/page.js';
+import { Composable, PageComponent, PageItemComponent } from './components/page/page.js';
 import { Component } from './components/component.js';
 
-// App 클래스는 전달받은 최고의 컨테이너 안에
 class App {
   private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
-    // 안에서 다른 어떤 다른 클래스를 만든다는 것은 사실 조금 위험함
-    // 이런 것들은 다 의존관계 주입(Dependency Injection)을 이용해서 
-    // 외부로부터 주입을 받는 것이 더 확장 가능하고 나중에 유닛 테스트하기에도 더 좋음
-    this.page = new PageComponent(); // 페이지라는 컴포넌트를 만들어서
-    this.page.attachTo(appRoot); // 만들어진 페이지를 appRoot에 추가해주고
+    // this.page = new PageComponent();
+    // 이제 어떤 PageItemComponent를 만들 수 있는지 타입을 알려주기
+    // PageComponent야 네가 만들 수 있는 PageItemComponent는 바로 이 클래스야
+    this.page = new PageComponent(PageItemComponent);
+    this.page.attachTo(appRoot);
 
-    // 생성자 안에는 아직 사용자에게 데이터를 받는 다이얼로그가 없으므로 임의
     const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-    this.page.addChild(image); // <- image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
     
     const video = new VideoComponent('Video Title', 'https://youtu.be/SNBaIAvKxU4');
-    this.page.addChild(video); // <- video.attachTo(appRoot, 'beforeend');
+    this.page.addChild(video);
     
     const note = new NoteComponent('Note Title', 'Note Body');
-    this.page.addChild(note); // <- note.attachTo(appRoot, 'beforeend');
+    this.page.addChild(note);
     
     const todo = new TodoComponent('Todo Title', 'Todo Item');
-    this.page.addChild(todo); // <- todo.attachTo(appRoot, 'beforeend');
+    this.page.addChild(todo);
   }
 }
 
-// querySelector은 기본적으로 Element 타입 또는 null을 리턴할 수 있는 아이
-// 하지만 이 클래스 document를 가지고 있는 요소를 동적으로 만드는 것이 아니라
-// 앱을 실행하면서 무언가가 변경되는 것이 아니라 개발할 때 정확하게 정해져 있기 때문에
-// 이런 경우엔 확신 있게 무조건 null 아니고 HTMLElement 타입이야라고 Type Assertion 사용할 수 있음
 new App(document.querySelector('.document')! as HTMLElement);
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -6,8 +6,23 @@ export interface Composable {
 
 type OnCloseListener = () => void;
 
-class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
-  private closeListener?: OnCloseListener;
+// 섹션을 감쌀 수 있는 컨테이너는 무조건 Component와 Composable 인터페이스를 구현해야 되고
+// 하나 더 추가적으로 setOnCloseListener라는 API가 있어야 한다라고 규격을 할 수 있음 
+interface SectionContainer extends Component, Composable {
+  setOnCloseListener(listener: OnCloseListener): void;
+}
+
+// 생성자를 정의하는 타입
+// 아무런 것도 전달받지 않는 생성자인데 이 생성자가 호출이 되면
+// SectionContainer를 만드는 그 어떤 클래스라도 괜찮다라고 타임을 정의
+// SectionContainer라는 이 interface의 규격을 따라가는 그 어떤 클래스라도 이 타입에 맞음
+type SectionContainerConstructor = {
+  new (): SectionContainer;
+}
+
+// 이제 PageItemComponent는 SectionContainer 하나만 구현하면 됨
+export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer {
+  private closeListener?: OnCloseListener; // 외부로부터 전달받은 콜백 함수를 저장하고 있을 변수
 
   constructor() {
     super(`<li class="page-item">
@@ -22,29 +37,30 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
     };
   }
   
-  // 외부에서 어떤 아이템을 전달하냐에 따라서 위 section 안에 추가
   addChild(child: Component) {
     const container = this.element.querySelector('.page-item__body')! as HTMLElement;
     child.attachTo(container);
   }
 
   setOnCloseListener(listener: OnCloseListener) {
-    this.closeListener = listener;
+    this.closeListener = listener; // 전달받은 listener로 등록
   }
 }
 
+// 이제 PageComponent는 정해진 어떤 특정한 클래스를 만드는 것이 아니라 
+// constructor에 전달된 SectionContainerConstruct 타입의 아이를 생성함
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
-  constructor() {
-    // super를 이용해 부모 클래스의 생성자를 호출
+  // 어떤 타입의 데이터를 만들 수 있는지 전달해서 내부에서만 볼 수 있는 private으로 설정
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page"></ul>');
   }
 
-  // Video가 될 수도 있고 Note가 될 수도 있고 아이템의 종류는 다양
   addChild(section: Component) {
-    const item = new PageItemComponent();
+    // const item = new PageItemComponent(); 한 가지만 내부에서 결정해서 만드는 것은 나쁨
+    // 내부에서 한 가지 클래스를 만드는 것이 아니라 외부에서 전달된 pageItemConstructor를 이용해 만듦 
+    const item = new this.pageItemConstructor();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
-    // Close가 클릭이 되면 지금 여기 페이지로부터 제거 
     item.setOnCloseListener(() => {
       item.removeFrom(this.element);
     });
